fd6936e709aae69691ee4d70441aa2fcd12308db
Sairus78 <83472347+Sairus78@users.noreply.github.com>
2024-09-14 09:47:54 +0000

    Deploying to gh-pages from @ Sairus78/mi-test-Repository@264769e57d61edd1881345dc936c24b8a82a6095 🚀

---
+++ page-1.js
@@ -0,0 +1,2 @@
+import"./assets/styles-C_trVBgP.js";document.querySelector(".form");document.addEventListener("click",function(e){console.log("Було натиснуто на елемент:",e.target),e.target.matches('input[type="text"][name="delay"]')&&console.log('Було натиснуто на поле "delay"')});
+//# sourceMappingURL=page-1.js.map
